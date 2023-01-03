4d61d4ab4a84415616581728f429ea2ee124ef81
elsymaharaniputri <114372763+elsymaharaniputri@users.noreply.github.com>
2023-01-03 10:13:52 +0700

    Add files via upload

---
+++ LAT2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="cssproject.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid d-block mx-auto ">
+        <div class="row justify-content-center text-white ">
+            <div class="col-4  mb-5 mt-5 ms-4 bg-dark py-3">
+                <p class="h2 ms-4 text-center">ISI DATA</p>
+                <div class="p-2 m-4  ">
+                    <form>
+                        <div class="mb-3 ">
+                            <label class="form-label">Nama Peralatan</label>
+                            <input type="text" class="form-control" placeholder="Nama Peralatan">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Jumlah</label>
+                            <input type="text" class="form-control" placeholder="Jumlah">
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Harga Satuan</label>
+                                <input type="text" class="form-control" placeholder="Harga Satuan">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Jumlah Harga</label>
+                                <input type="text" class="form-control" placeholder="Jumlah Harga">
+                            </div>
+                        </div>
+                        <sub>
+                            <div>
+                                <button class="w-50 btn btn-primary d-block mx-auto" type="button">Submit</button>
+                            </div>
+                        </sub>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- KOLOM GAMBAR -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"
+        integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
